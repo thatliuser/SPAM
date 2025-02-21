97fed19cf0fb3b8e86a045e8ed984e7f559608c7
Jacob Lee <jacoblee0001@gmail.com>
2025-02-21 00:05:14 -0800

    Add functionality for setting cloudinit configuration after cloning vm

--- clone.py
+++ clone.py
@@ -1,6 +1,7 @@
 from proxmoxer import ProxmoxAPI
 import conf.config as config
 import utils
+import cloudinit
 
 def clone_vm(prox: ProxmoxAPI, source_node: str, source_id: str, **kwargs) -> None:
     print(f"Cloning VMID {source_id} in {source_node} to VMID {kwargs['newid']} in {kwargs['target']}")
@@ -14,6 +15,8 @@ def clone_env(prox: ProxmoxAPI, env: config.Env) -> None:
         for box in env.boxes:
             for _ in range(box.copies):
                 clone_vm(prox, env.template_node, box.id, newid = newid, target=node, **box.config)
+                if box.cloud:
+                    cloudinit.set_cloudinit(prox, node, newid, **box.cloud)
                 newid += 1
 
 # prox.nodes('cuci-r730-pve01').qemu('7101').clone.create(newid='28006',target='cuci-r730-pve03')
---
+++ cloudinit.py
@@ -0,0 +1,8 @@
+from proxmoxer import ProxmoxAPI
+import conf.config as config
+import utils
+
+def set_cloudinit(prox: ProxmoxAPI, node: str, vmid: int, **kwargs) -> None:
+    print(f"Setting cloudinit for VMID {vmid} in {node} {kwargs}")
+    prox.nodes(node).qemu(vmid).config.set(**kwargs)
+    return
