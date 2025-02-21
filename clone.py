73ea6e01538b962d0ef8c82214b8540a921e6c72
Jacob Lee <jacoblee0001@gmail.com>
2025-02-21 00:21:40 -0800

    Add basic error handling in clone_vm

--- clone.py
+++ clone.py
@@ -1,12 +1,18 @@
 from proxmoxer import ProxmoxAPI
+from proxmoxer.core import ResourceException
 import conf.config as config
 import utils
 import cloudinit
 
+
 def clone_vm(prox: ProxmoxAPI, source_node: str, source_id: str, **kwargs) -> None:
-    print(f"Cloning VMID {source_id} in {source_node} to VMID {kwargs['newid']} in {kwargs['target']}")
-    task_id = prox.nodes(source_node).qemu(source_id).clone.create(**kwargs)
-    utils.block_until_done(prox, task_id, source_node)
+    try:
+        task_id = prox.nodes(source_node).qemu(source_id).clone.create(**kwargs)
+        target = source_node if 'target' not in kwargs else kwargs['target']
+        print(f"Cloning VMID {source_id} in {source_node} to VMID {kwargs['newid']} in {target}")
+        utils.block_until_done(prox, task_id, source_node)
+    except Exception as e:
+        print(e)
     return
 
 def clone_env(prox: ProxmoxAPI, env: config.Env) -> None:
@@ -14,9 +20,7 @@ def clone_env(prox: ProxmoxAPI, env: config.Env) -> None:
     for node in env.nodes:
         for box in env.boxes:
             for _ in range(box.copies):
-                clone_vm(prox, env.template_node, box.id, newid = newid, target=node, **box.config)
+                clone_vm(prox, env.template_node, box.id, newid = newid, target=node,**box.config)
                 if box.cloud:
                     cloudinit.set_cloudinit(prox, node, newid, **box.cloud)
                 newid += 1
-
-# prox.nodes('cuci-r730-pve01').qemu('7101').clone.create(newid='28006',target='cuci-r730-pve03')
